a4adc91d52217114bbbcaa9fb9b0f75c9355a9b8
AK Krajewska <akrajewska@gmail.com>
2017-08-16 13:37:26 -0700

    F/C toggle

--- resources/main.js
+++ resources/main.js
@@ -1,3 +1,33 @@
+var faren = false;
+
+
+/*
+function displayTemp(cTemp, faren){
+    if(faren) return (cTemp * 9/5 + 32) + " F";
+    return cTemp + " C";
+} */
+
+function displayTemp (cTemp, f){
+    if(f) return (cTemp * 9/5 + 32) + " F";
+    return cTemp + " C";
+}
+
+function render(wd, faren){
+   var currentLocation = wd.name;
+   var currentWeather = wd.weather[0].description;  
+   var currentTemp = displayTemp(wd.main.temp, faren);
+   var high = displayTemp(wd.main.temp_min, faren);
+   var low = displayTemp(wd.main.temp_max, faren);
+   var icon = wd.weather[0].icon;
+            
+    $('#currentLocation').html(currentLocation);
+    $('#currentWeather').html(currentWeather);
+    $('#currentTemp').html(currentTemp);
+    $('#high').html(high);
+    $('#low').html(low);
+    $('#icon').html('<img src="' + icon + '">');
+}
+
 $(function(){
     
     var loc;
@@ -6,32 +36,19 @@ $(function(){
         console.log("assigning the data...");
         loc = d.loc.split(",");
         console.log(loc);
-    
-        // call to the weather API
-        //$('#location').text(loc);
-        
-        $.getJSON('https://fcc-weather-api.glitch.me/api/current?lat=' + loc[0] + '&lon=' + loc[1], function(wd){
-            console.log("got the data " + wd);
-            console.log(wd.name); //the city
-            
-            var currentLocation = wd.name;
-            var currentWeather = wd.weather[0].description;
-            console.log(currentWeather);
-            var currentTemp = wd.main.temp;
-            console.log(currentTemp);
-            var high = wd.main.temp_min;
-            var low = wd.main.temp_max;
-            var icon = wd.weather[0].icon;
-            
-            $('#currentLocation').html(currentLocation);
-            $('#currentWeather').html(currentWeather);
-            $('#currentTemp').html(currentTemp);
-            $('#high').html(high);
-            $('#low').html(low);
-            $('#icon').html('<img src="' + icon + '">');
 
+        $.getJSON('https://fcc-weather-api.glitch.me/api/current?lat=' + loc[0] + '&lon=' + loc[1], function(apiData){
+            wd = apiData;
+
+            render(apiData, faren);
+
+            $('#toggle').click(function(){
+                    faren = !faren;
+                    console.log(faren);
+                    render(wd, faren);
+            });          
 
         });
       
-    })
+    });
 })
